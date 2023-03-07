03331c5c688ce650a48f8afcbc74da22b8361a4b
Samuel-fps <samuel.fernandezpeinado@gmail.com>
2023-03-07 10:49:26 +0100

    Inicial P1

---
+++ P1/EJERCICIOS/ejeciciosP1.h
@@ -0,0 +1,15 @@
+#include "AbinV.h"
+
+// EJERCICIO 1
+
+// EJERCICIO 2
+
+// EJERCICIO 3
+
+// EJERCICIO 4
+
+// EJERCICIO 5
+
+// EJERCICIO 6
+
+// EJERCICIO 7
---
+++ P1/EJERCICIOS/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include <ejerciciosP1.h>
+
+int main(){
+	Abin<char> A = Abin();
+	A.insertarRaiz('A');
+	A.insertarHijoDrcho(A.raiz(), 'B');
+	A.eliminarHijoIzqdo(A.raiz(), 'C');
+	
+	A.
+	
+	return 0;
+}
