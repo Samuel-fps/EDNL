ccd9646939211e311d06592635ee56c6adcb3bd3
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-04-25 00:55:45 +0200

    intento blancoNegro

--- EXAMEN/README.md
+++ EXAMEN/README.md
@@ -73,6 +73,8 @@ bool esAvl(const Abin<T>& A){
 
 ### Parcial 1 - Agenda
 
+Haz una agenda usando un Arbol de búsqueda. Utiliza como estructura una con los campos telefono y nombre por ejemplo y acuerdate de sobrecargar los operandos de comparación de la estructura
+
 ```cpp
 
 ```
--- P5_Apo/ej5_blancoNegro.cpp
+++ P5_Apo/ej5_blancoNegro.cpp
@@ -1,4 +1,4 @@
-#include "../TAD_APO/apo.h" // Importamos el arbol binario
+#include "../TAD_AGEN/agenlis.h" // Importamos el arbol binario
 using namespace std;
 
 //////////// Esta parte es solo para poder leer los arboles de los ficheros
@@ -13,14 +13,62 @@ const tElto fin = '#';
 /*  EJERCICIO 5 - Blanco y Negro
 
     Implementa una función que dado un árbol de esta clase, con k+1 niveles,
-    devuelva la figura asociada, representada como una matriz cuadrada de tamaño 2^4
+    devuelva la figura asociada, representada como una matriz cuadrada de tamaño 2^K
     en la que cada celda representa un punto blanco o negro. 
 
 */
 
+#include <vector>
+#include <algorithm>
+
+template <typename T>
+int profundidadAgen(const Agen<T>& A, const typename Agen<T>::nodo n){
+    if(n == Agen<T>::NODO_NULO){
+        return 0;
+    }
+    else{
+        Agen<char>::nodo hijo = I.hijoIzqdo(n);
+        int profMax = 0;
+        while(hijo != Agen<T>::NODO_NULO){
+            profMax = std::max(profMax, profundidadAgen(A, hijo) + 1);
+            hijo = I.hermDrcho(hijo);
+        }
+        return profMax;
+    }
+}
+
+void dibujar(int){
+
+}
+
+
+void figuraMatriz_rec(const Agen<char>& I, vector<vector<char>>& m, const typename Agen<char>::nodo n, const int nivel){
+    if(n != Agen<char>::NODO_NULO){
+        if(I.elemento(n) == ' '){
+            Agen<char>::nodo hijo = I.hijoIzqdo(n);
+            while(hijo != Agen<char>::NODO_NULO){
+                figuraMatriz_rec(I, m, hijo, nivel + 1);
+                hijo = I.hermDrcho(hijo);
+            }
+        }
+        else if(I.elemento(n) == 'B'){
+            
+        }
+        else if(I.elemento(n) == 'N'){
+
+        }
+    }
+}
+
+template <typename T = char>
+vector<vector<T>> figuraMatriz(Agen<T> I){
+    vector<vector<T>> matriz;
+    figuraMatriz_rec(I, matriz, I.raiz(), 0);
+    return matriz;
+}
 
 int main(){
-    Apo<tElto> A();
+    Agen<tElto> A();
 
     ifstream fa("AbbA.dat"); // Abrir fichero de entrada.
     //rellenarAbb(fa, A); // Desde fichero.
