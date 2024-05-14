65b34c885013bd474c105e03d621b40fbdcfa4f3
Samuel-fps <samuel.fernandezpeinado@gmail.com>
2024-05-14 12:16:05 +0200

    primer planteamiento

--- P7_grafos2/ej7_rutaMinima.cpp
+++ P7_grafos2/ej7_rutaMinima.cpp
@@ -5,8 +5,47 @@
 
 /*  EJERCICIO 7
 
-    
+    Se dispone de dos grafos (matriz de costes) que representan los costes de viajar entre
+    N ciudades españolas utilizando el tren (primer grafo) y el autobús (segundo grafo).
+    Ambos grafos representan viajes entre las mismas N ciudades.
+
+    Nuestro objetivo es hallar el camino de coste mínimo para viajar entre dos ciudades
+    concretas del grafo, origen y destino, en las siguientes condiciones:
+
+        - La ciudad origen sólo dispone de transporte por tren.
+        - La ciudad destino sólo dispone de transporte por autobús.
+        - El sector del taxi, bastante conflictivo en nuestros problemas, sigue en huelga, por lo que únicamente es posible cambiar de transporte en dos ciudades del grafo, cambio1 y cambio2, donde las estaciones de tren y autobús están unidas.
+
+    Implementa un subprograma que calcule la ruta y el coste mínimo para viajar entre
+    las ciudades Origen y Destino en estas condiciones.
 */
+
+template <typename tCoste> 
+double rutaCosteMin(const GrafoP<tCoste>& Tren,
+                    const GrafoP<tCoste>& Bus,
+                    const GrafoP<tCoste>::vertice origen,
+                    const GrafoP<tCoste>::vertice destino,
+                    const GrafoP<tCoste>::vertice cambio1,
+                    const GrafoP<tCoste>::vertice cambio2)
+{
+    typedef GrafoP<tCoste>::vertice vertice;
+    size_t N = Tren.numVert();
+
+    GrafoP<tCoste> Costes;
+
+    for(vertice i = 0 ; i < N ; i++){
+        Costes[origen][i] = Tren[origen][i]; // Origen solo tren
+        Costes[i][destino] = Bus[i][destino]; // Destino solo bus
+        for(vertice j = 0 ; j < N ; j++){
+            if(i == cambio1)
+                Costes[i][j] = std::min(minBus[i][j], minTren[i][j]);
+            if(i == cambio2)
+                Costes[i][j] = std::min(minBus[i][j], minTren[i][j]);
+        }
+    }
+
+    matriz<tCoste> Ruta;
+}
 
 
 int main() {
