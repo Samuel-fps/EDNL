feb8caba811a2f2340a6daf6f165b112b0d8248a
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-15 18:54:55 +0200

    ej8 terminado

--- P7_grafos2/ej8_tarifaMinima.cpp
+++ P7_grafos2/ej8_tarifaMinima.cpp
@@ -25,20 +25,39 @@
 */
 
 /*
-Diskstra(Tren) + inverso (bus)
-Diskstra(bus) + inverso (tren)
+Dijkstra(Tren) + inverso (bus)
+Dijkstra(bus) + inverso (tren)
 No es necesario solo tren o solo bus porque es un caso especifico de las otras dos opciones
+Si no cambias de trasporte el transbordo es al final y la suma es coste_origen_transbordo + 0 (coste de ir transbordo_destino)
 */
 
 template <typename tCoste> 
-double rutaCosteMin(const GrafoP<tCoste>& Tren,
+double tarifaMinima(const GrafoP<tCoste>& Tren,
                     const GrafoP<tCoste>& Bus,
                     const typename GrafoP<tCoste>::vertice origen,
                     const typename GrafoP<tCoste>::vertice destino)
 {
     typedef GrafoP<tCoste>::vertice vertice;
-    size_t N = Tren.numVert();
+
+    vector<vertice> ruta; // No nos pide la ruta por lo que usamos el mismo vector
+    vector<tCoste> origenTren = Dijkstra(Tren, origen, ruta),
+                   destinoTren = DijkstraInv(Tren, origen, ruta),
+                   origenBus = Dijkstra(Bus, origen, ruta),
+                   destinoBus = DijkstraInv(Bus, origen, ruta);
+
+    // Camino con transbordo de minimo coste tren->bus
+    tCoste<tCoste> minTrenBus = suma(origenTren[0], destinoBus[0]);
+    for(size_t i = 0 ; i < origenTren.size() ; i++)
+        if(minTrenBus < suma(origenTren[i], destinoBus[i])) // Hacer transbordo en i es menos costoso
+            minTrenBus = suma(origenTren[i], destinoBus[i]);
+
+    // Camino con transbordo de minimo coste bus->tren
+    tCoste<tCoste> minBusTren = suma(origenBus[0], destinoTren[0]);
+    for(size_t i = 0 ; i < origenBus.size() ; i++)
+        if(minBusTren < suma(origenBus[i], destinoTren[i])) // Hacer transbordo en i es menos costoso
+            minBusTren = suma(origenBus[i], destinoTren[i]);
     
+    return std::min(minTrenBus, minBusTren);
 }
 
 int main() {
