94110cf6034f12dcf59ce2cc0c029eab3145050e
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-16 13:18:54 +0200

    ej10 terminado

--- P7_grafos2/ej10_minOrgDest2.cpp
+++ P7_grafos2/ej10_minOrgDest2.cpp
@@ -5,8 +5,58 @@
 
 /*  EJERCICIO 10
 
-    
+    Se dispone de tres grafos que representan la matriz de costes para viajes en un determinado país, pero por diferentes medios de
+    transporte (tren, autobús y avión).
+    Por supuesto los tres grafos tendrán el mismo número de nodos, N.
+
+    Dados los siguientes datos:
+
+        - los tres grafos,
+        - una ciudad de origen,
+        - una ciudad de destino,
+        - el coste del taxi para cambiar, dentro de una ciudad, de la estación de tren a la de autobús o viceversa(taxi-tren-bus) y
+        - el coste del taxi desde el aeropuerto a la estación de tren o la de autobús, o viceversa (taxi-aeropuerto-tren/bus)
+
+    y asumiendo que ambos costes de taxi (distintos entre sí, son dos costes diferentes) son
+    constantes e iguales para todas las ciudades, implementa un subprograma que calcule el
+    camino y el coste mínimo para ir de la ciudad origen a la ciudad destino.
 */
+
+template <typename tCoste> 
+tCoste rutaCosteMin(const GrafoP<tCoste>& Tren,
+                    const GrafoP<tCoste>& Bus,
+                    const GrafoP<tCoste>& Avion,
+                    const typename GrafoP<tCoste>::vertice origen,
+                    const typename GrafoP<tCoste>::vertice destino,
+                    const tCoste taxiBusTren,
+                    const tCoste taxiAvion,
+                    vector<typename GrafoP<tCoste>::vertice>& caminoCosteMin)
+{
+    typedef GrafoP<tCoste>::vertice vertice;
+    size_t N = Tren.numVert();
+    GrafoP<tCoste> G(3*N);
+
+    // Rellenamos matriz de costes para aplicar Dijkstra
+    for(vertice i=0 ; i < N ; i++){
+        G[i]  [i+n] = taxiBusTren;      // Tren -> Bus
+        G[i+n][i] = taxiBusTren;        // Bus  -> Tren
+
+        G[i]    [i+n+n] = taxiAvion;    // Tren -> Avion
+        G[i+n]  [i+n+n] = taxiAvion;    // Bus -> Avion
+        G[i+n+n][i]     = taxiAvion;    // Avion -> Tren
+        G[i+n+n][i+n]   = taxiAvion;    // Avion -> Bus
+
+        for(vertice j=0 ; j < N ; j++){
+            G[i][j] = Tren[i][j];           // Tren -> Tren 
+            G[i+n][j+n] = Bus[i][j];        // Bus  -> Bus 
+            G[i+n+n][j+n+n] = Bus[i][j];    // Avion  -> Avion
+        }
+    }
+
+    vector<tCoste> costesMin = Dijkstra(G, origen, caminoCosteMin);
+
+    return costesMin[destino];
+}
 
 
 int main() {
