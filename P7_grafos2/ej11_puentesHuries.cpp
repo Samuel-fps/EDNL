67a34df6934f778c1913fd910046f8153fa5a8f5
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-20 16:57:01 +0200

    ej11 hecho a espera de corregir

--- P7_grafos2/ej11_puentesHuries.cpp
+++ P7_grafos2/ej11_puentesHuries.cpp
@@ -1,7 +1,7 @@
 #include "../TAD_GRAFO/alg_grafoPMC.h"
 #include "alg_grafo_E-S.h"
 #include <iostream>
-#include <vector>;
+#include <vector>
 
 /*  EJERCICIO 11
 
@@ -21,6 +21,48 @@
     de viajar entre cualesquiera dos ciudades de estas tres islas.
     ¡¡¡ QUE DISFRUTÉIS EL VIAJE !!!
 */
+
+typedef struct {
+    size_t origen, destino;
+} Puente;
+
+template <typename tCoste>
+matriz<tCoste> puentesHuries(const GrafoP<tCoste>& I1,
+                             const GrafoP<tCoste>& I2,
+                             const GrafoP<tCoste>& I3,
+                             const vector<Puente> Puentes)
+{
+    typedef GrafoP<tCoste>::vertice vertice;
+    size_t n1 = I1.numVert(), n2 = I2.numVert(), n3 = I3.numVert(); // Número de ciudades en cada isla
+    size_t n = n1 + n2 + n3; // Número de ciudades totales
+    GrafoP<tCoste> A(n);
+
+    // Coste 0 de viajar por los puentes
+    for(Puente p : Puentes){
+        A[p.origen][p.destino] = 0;
+        A[p.destino][p.origen] = 0;
+    }
+
+    // Rellenar con la matriz de costes isla 1
+    for(vertice i=0 ; i < n1 ; i++)
+        for(vertice j=0 ; j < I.numVert() ; j++)
+            A[i][j] = I1[i][j];
+
+    // Rellenar con la matriz de costes isla 2
+    for(vertice i=n1  ; i < n1+n2 ; i++)
+        for(vertice j=0 ; j < I.numVert() ; j++)
+            A[i][j] = I2[i][j];
+
+    // Rellenar con la matriz de costes isla 3
+    for(vertice i=n1+n2 ; i < n1+n2+n3 ; i++)
+        for(vertice j=0 ; j < I.numVert() ; j++)
+            A[i][j] = I3[i][j];
+    
+    matriz<vertice> P;
+    matriz<tCoste> Costes = Floyd(A, P);
+
+    return A;
+}
 
 
 int main() {
