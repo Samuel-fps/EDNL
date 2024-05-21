96927db801cf273e198666a06aa115797839a114
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-21 13:15:44 +0200

    corrección espacios

--- P8_grafos3/ej1_tombuctuDistribucion.cpp
+++ P8_grafos3/ej1_tombuctuDistribucion.cpp
@@ -6,9 +6,9 @@
 /*  EJERCICIO 1
 
     El archipiélago de Tombuctú, está formado por un número indeterminado de islas, cada una de las
-    cuales tiene, a su vez, un número indeterminado de  ciudades.  En cambio, sí es conocido el 
+    cuales tiene, a su vez, un número indeterminado de ciudades. En cambio, sí es conocido el 
     número total de ciudades de Tombuctú (podemos llamarlo N, por ejemplo). Dentro de cada una de 
-    las islas existen carreteras que  permiten viajar entre todas las ciudades de la isla. Se 
+    las islas existen carreteras que permiten viajar entre todas las ciudades de la isla. Se 
     dispone de las coordenadas cartesianas (x, y) de todas y cada una de las ciudades del 
     archipiélago. Se dispone de un grafo (matriz de adyacencia) en el que se indica si existe 
     carretera directa entre cualesquiera dos ciudades del archipiélago. El objetivo de nuestro 
@@ -20,7 +20,7 @@
         - Lista de ciudades de Tombuctú representada cada una de ellas por sus coordenadas cartesianas.
         - Matriz de adyacencia de Tombuctú, que indica las carreteras existentes en dicho archipiélago.
 
-    Implementen  un subprograma que calcule  y devuelva la distribución en islas de las ciudades de 
+    Implementen un subprograma que calcule y devuelva la distribución en islas de las ciudades de 
     Tombuctú, así como el coste mínimo de viajar entre cualesquiera dos ciudades de una misma isla del
     archipiélago.    
 */
