6e70b0766b5927dbcd886ff3f871861f3ca305bd
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-24 13:28:37 +0200

    ej5 hecho

--- P8_grafos3/ej5_fibra.cpp
+++ P8_grafos3/ej5_fibra.cpp
@@ -12,20 +12,19 @@
 */
 
 template <typename tCoste>
-void constriurPuente()
+tCoste constriurPuente(cosnt GrafoP<tCoste>& distancias)
 {
-    typedef GrafoP<tCoste>::vertice vertice;
-    
-}
+    size_t n = distancias.numVert();
 
-int main() {
-    GrafoP<int> grafo("GrafoA.txt");
+    GrafoP<tCoste> min = kruskal(distancias);
 
-    std::cout << grafo;
+    // sumar distancia de arbol de coste mínimo
+    // Como el coste de ir de i a j es igual que de j a i no basta con sumar una de las diagonales, en este caso la superior -> j=i
+    // El coste de la diagonal principal será 0 por lo que también nos lo podemos ahorrar -> j=i+1
+    tCoste distancia=0;
+    for(size_t i=0 ; i < n ; i++)
+        for(size_t j=i+1 ; j < n ; j++)
+            distancia += min[i][j];
 
-    // LLamada a funcion de ejercicio
-
-    // Imprimir el resultado
-
-    return 0;
+    return distancia;
 }
