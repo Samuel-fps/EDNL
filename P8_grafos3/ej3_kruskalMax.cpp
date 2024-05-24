9ec0969246aca036bda1d90afb12b0b30d0b6f19
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-24 12:39:40 +0200

    ej3 terminado

--- P8_grafos3/ej3_kruskalMax.cpp
+++ P8_grafos3/ej3_kruskalMax.cpp
@@ -7,25 +7,46 @@
 
     Implementa un subprograma para encontrar un árbol de extensión máximo. 
     ¿Es más difícil que encontrar un árbol de extensión mínimo?
-*/
 
-// Tiene la misma complejidad el maximo que el minimo
+    SOLUCIÓN
+        - Tiene la misma complejidad el maximo que el minimo.
+        - En este caso usaría el mismo algoritmo de kruskal pero usando un APO invertido que tenga
+        el máximo en la cima, de esta forma el algoritmo cogería siempre la arista de mayor coste.
+*/
 
 template <typename tCoste>
 void kruskalMax()
+// Devuelve un árbol generador de coste máximo
+// de un grafo no dirigido ponderado y conexo G.
 {
-    typedef GrafoP<tCoste>::vertice vertice;
+    assert(!G.esDirigido());
+
+   typedef typename GrafoP<tCoste>::vertice vertice;
+   typedef typename GrafoP<tCoste>::arista arista;
+   const tCoste INFINITO = GrafoP<tCoste>::INFINITO;
+   const size_t n = G.numVert();
+   GrafoP<tCoste> g(n);     // Árbol generador de coste máximo
+   Particion P(n);          // Partición inicial del conjunto de vértices de G.
+   Apo<arista> A(n*n);      // Aristas de G ordenadas por costes teniendo max en la cima.
     
-}
+    // Copiar aristas del grafo G en el APO A.
+   for (vertice u = 0; u < n; u++)
+      for (vertice v = u+1; v < n; v++)
+         if(G[u][v] != INFINITO)
+            A.insertar(arista(u, v, G[u][v]));
 
-int main() {
-    GrafoP<int> grafo("GrafoA.txt");
-
-    std::cout << grafo;
-
-    // LLamada a funcion de ejercicio
-
-    // Imprimir el resultado
-
-    return 0;
+   size_t i = 1;
+   while (i <= n-1) {   // Seleccionar n-1 aristas.
+      arista a = A.cima(); // arista de mayor coste
+      A.suprimir();
+      vertice u = P.encontrar(a.orig);
+      vertice v = P.encontrar(a.dest);
+      if (u != v) { // Los extremos de a pertenecen a componentes distintas
+         P.unir(u, v);
+         // Incluir la arista a en el árbol g
+         g[a.orig][a.dest] = g[a.dest][a.orig] = a.coste;
+         i++;
+      }
+   }
+   return g;
 }
