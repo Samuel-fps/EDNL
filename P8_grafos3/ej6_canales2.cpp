6560ac031bc0a11b700424b984a020a6e8f02dbf
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-26 13:13:20 +0200

    ej6 terminado

--- P8_grafos3/ej6_canales2.cpp
+++ P8_grafos3/ej6_canales2.cpp
@@ -9,7 +9,9 @@
     teniendo en cuenta las siguientes premisas:
 
         - El coste de abrir cada nuevo canal es casi prohibitivo, luego la solución final debe tener un número mínimo de canales.
-        - El Ministerio de Fomento nos subvenciona por m3/sg de caudal, luego el conjunto de los canales debe admitir el mayor caudal posible, pero por otra parte, el coste de abrir cada canal es proporcional a su longitud, por lo que el conjunto de los canales  también debería medir lo menos posible. Así pues, la solución óptima debería combinar adecuadamente ambos factores.
+        - El Ministerio de Fomento nos subvenciona por m3/sg de caudal, luego el conjunto de los canales debe admitir el mayor caudal posible,
+          pero por otra parte, el coste de abrir cada canal es proporcional a su longitud, por lo que el conjunto de los canales también 
+          debería medir lo menos posible. Así pues, la solución óptima debería combinar adecuadamente ambos factores.
 
     Dada la matriz de distancias entre las diferentes ciudades del valle del Jerte, otra matriz con los 
     diferentes caudales máximos admisibles entre estas ciudades teniendo en cuenta su orografía, la subvención
@@ -18,20 +20,19 @@
 */
 
 template <typename tCoste>
-void constriurPuente()
+GrafoP<tCoste> constriurPuente(const GrafoP<tCoste>& distancias,
+                               const GrafoP<tCoste>& caudales,
+                               const double subvencion,
+                               const double costeKm)
 {
     typedef GrafoP<tCoste>::vertice vertice;
-    
-}
+    size_t n = distancias.numVert();
+    GrafoP<tCoste> caudalKm(n),
+                   canales(n);
 
-int main() {
-    GrafoP<int> grafo("GrafoA.txt");
+    for(vertice i=0 ; i < n ; i++)
+        for(vertice j=0 ; j < n ; j++)
+            caudalKm[i][j] = caudalKm[j][i] = distancias[i][j] * costeKm - caudales[i][j] * subvencion;
 
-    std::cout << grafo;
-
-    // LLamada a funcion de ejercicio
-
-    // Imprimir el resultado
-
-    return 0;
+    return kruskall(caudalKm);
 }
