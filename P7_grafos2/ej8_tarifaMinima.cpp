de17ba88d5489314a63288f354fadcbf0c090922
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-29 18:37:58 +0200

    correccion tipo

--- P7_grafos2/ej8_tarifaMinima.cpp
+++ P7_grafos2/ej8_tarifaMinima.cpp
@@ -32,7 +32,7 @@ Si no cambias de trasporte el transbordo es al final y la suma es coste_origen_t
 */
 
 template <typename tCoste> 
-double tarifaMinima(const GrafoP<tCoste>& Tren,
+tCoste tarifaMinima(const GrafoP<tCoste>& Tren,
                     const GrafoP<tCoste>& Bus,
                     const typename GrafoP<tCoste>::vertice origen,
                     const typename GrafoP<tCoste>::vertice destino)
@@ -46,13 +46,13 @@ double tarifaMinima(const GrafoP<tCoste>& Tren,
                    destinoBus = DijkstraInv(Bus, origen, ruta);
 
     // Camino con transbordo de minimo coste tren->bus
-    tCoste<tCoste> minTrenBus = suma(origenTren[0], destinoBus[0]);
+    tCoste minTrenBus = suma(origenTren[0], destinoBus[0]);
     for(size_t i = 0 ; i < origenTren.size() ; i++)
         if(minTrenBus < suma(origenTren[i], destinoBus[i])) // Hacer transbordo en i es menos costoso
             minTrenBus = suma(origenTren[i], destinoBus[i]);
 
     // Camino con transbordo de minimo coste bus->tren
-    tCoste<tCoste> minBusTren = suma(origenBus[0], destinoTren[0]);
+    tCoste minBusTren = suma(origenBus[0], destinoTren[0]);
     for(size_t i = 0 ; i < origenBus.size() ; i++)
         if(minBusTren < suma(origenBus[i], destinoTren[i])) // Hacer transbordo en i es menos costoso
             minBusTren = suma(origenBus[i], destinoTren[i]);
