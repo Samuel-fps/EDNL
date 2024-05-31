8b57b8b6daeb706826a9478f24a6a99ea7af6041
Samuel-fps <samuel.fernandezpeinado@gmail.com>
2024-05-31 17:27:19 +0200

    bucles creados

--- EXAMEN/parcial2_23_laberinto.cpp
+++ EXAMEN/parcial2_23_laberinto.cpp
@@ -5,16 +5,37 @@ typedef struct {
 } Casilla;
 
 template <typename tCoste>
+GrafoP<tCoste> CasillaToNodo(const Casilla& c, int N){
+    return c.x * N + c.y;
+}
+
+template <typename tCoste>
+Casilla nodoToCasilla(const GrafoP<tCoste>::vertice& n, int N){
+    Casilla c;
+    c.x = n / N;
+    c.y = n % N;
+    return c;
+}
+
+template <typename tCoste>
 tCoste numSaltos(const int n, const int m,
                  const vector<Casilla>& trampas,
                  const vector<Casilla>& caballeros)
 {
     typedef GrafoP<tCoste>::vertice vertice;
     tCoste nSaltos=0;
-    size_t N = n*m; 
+    size_t N = n*m;
+
+    for(Casilla t : trampas){
+        vertice trampa = CasillaToNodo(t, N);
+    }
 
     GrafoP<tCoste> L(N);
-    for(vertice i=0 ; i < )
+    for(vertice i=0 ; i < N ; i++){
+        for(vertice j=0 ;j < N ; j++){
+            
+        }
+    }
 
     return nSaltos;
 }
