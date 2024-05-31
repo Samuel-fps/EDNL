ba155b0652f8cc3b19e6e5f5461360a3acdf7652
Samuel <86070772+Samuel-fps@users.noreply.github.com>
2024-05-31 18:35:27 +0200

    terminado ej examen laberinto

--- EXAMEN/parcial2_23_laberinto.cpp
+++ EXAMEN/parcial2_23_laberinto.cpp
@@ -17,28 +17,52 @@ Casilla nodoToCasilla(const GrafoP<tCoste>::vertice& n, int N){
     return c;
 }
 
+bool alcanzable(const Casilla& fauno, const Casilla& c){
+    return abs(fauno.x - c.x) + abs(fauno.y - c.y) == 3 
+            && fauno.x != c.x  
+            && fauno.y != c.y;
+}
+
+bool ady(const Casilla& c1, const Casilla& c2){
+    return abs(c1.x - c2.x) + abs(c1.y - c2.y) == 1;
+}
+
 template <typename tCoste>
 tCoste numSaltos(const int n, const int m,
                  const vector<Casilla>& trampas,
                  const vector<Casilla>& caballeros)
 {
     typedef GrafoP<tCoste>::vertice vertice;
-    tCoste nSaltos=0;
     size_t N = n*m;
 
     GrafoP<tCoste> L(N);
-    for(vertice i=0 ; i < N ; i++){
-        for(vertice j=0 ;j < N ; j++){
-            L[i][j] = 1;
+    for(vertice i=0 ; i < N ; i++){ // O(n^3)
+        for(vertice j=0 ; j < N ; j++){
+            if(alcanzable(CasillaToNodo(i), casillaToNodo(j))){ // Esta dentro de los movimientos del cababallo
+                L[i][j] = 1;
+                // ver si la casilla destino es adyacente a un caballero
+                for(Casilla caballero : caballeros){
+                    if(caballero, ady(nodoToCasilla(j))){
+                        L[i][j] = GrafoP<tCoste>::INFINITO;
+                    }
+                }
+            }
+
         }
     }
 
-    for(Casilla t : trampas){ // O(n^2)
-        vertice trampa = CasillaToNodo(t, N);
+    for(Casilla trampa : trampas){ // O(n^2)
+        vertice nTrampa = CasillaToNodo(trampa, N);
         for(vertice i=0 ; i < N ; i++){
-            L[trampa][i] = L[i][trampa] = GrafoP<tCoste>::INFINITO;
+            L[nTrampa][i] = L[i][nTrampa] = GrafoP<tCoste>::INFINITO;
         }
     }
 
-    return nSaltos;
+    vertice origen = CasillaToNodo(Casilla(0,0)),
+            destino = CasillaToNodo(Casilla(n-1,m-1));
+
+    vector<vertice> P;
+    vector<tCoste> costes = Dijkstra(L, origen, P);
+
+    return costes[destino];
 }
